7bf1979d4301ae15f63544605c82194533c93dbc
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-10-25 17:12:43 -0700

    Added a random policy to keyboard_pong.

--- src/keyboard_pong.py
+++ src/keyboard_pong.py
@@ -5,24 +5,25 @@ enter 0, 1, 2, 3, 4, or 5 on stdin to make an action.
 
 import argparse
 import random
+import time
 
 import tensorflow as tf
 import numpy as np
 
 import atari_env
+import utils
 
-def _read_action_from_stdin():
-    allowable_actions = ["0", "1", "2", "3", "4", "5"]
-    action = input()
-    while action not in allowable_actions:
-        print("Error: enter one of {}.".format(allowable_actions))
-        action = input()
-    return int(action)
+def _delayed_policy(policy, delay):
+    def _policy(obs):
+        time.sleep(delay)
+        return policy(obs)
+    return _policy
 
 def main():
     """Plays pong with the keyboard."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--random_policy', action="store_true")
     args = parser.parse_args()
 
     seed = args.seed
@@ -30,13 +31,18 @@ def main():
     np.random.seed(seed)
     random.seed(seed)
     env = atari_env.gen_pong_env(args.seed)
+    if args.random_policy:
+        random_policy = utils.create_random_policy(env)
+        policy = _delayed_policy(random_policy, 0.1)
+    else:
+        policy = utils.create_keyboard_policy(env)
 
-    _obs = env.reset()
+    obs = env.reset()
     env.render()
     done = False
     while not done:
-        act = _read_action_from_stdin()
-        _obs, _reward, done, _info = env.step(act)
+        action = policy(np.array([obs]))[0]
+        obs, _reward, done, _info = env.step(action)
         env.render()
 
 if __name__ == "__main__":
